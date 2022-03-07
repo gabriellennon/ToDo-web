0af8afe53694b27cc5d9405327f2791a528f6802
frontend <gabriellennon@wizsolucoes.com.br>
2022-03-06 21:01:33 -0300

    remove task complete

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -44,7 +44,8 @@ export function TaskList() {
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const tasksFilter = tasks.filter((task) => task.id !== id);
+    setTasks([...tasksFilter]);
   }
 
   function generateId(){
